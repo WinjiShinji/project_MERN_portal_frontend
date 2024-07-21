526a416350bd5a2602f7a90f0fb89ec8c8677e80
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-07-21 10:48:00 +0100

    typo

--- src/utility/axiosConfig.js
+++ src/utility/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = "https://project-mern-portal-back-end.onrender.com/"
+const BASE_URL = "https://project-mern-portal-back-end.onrender.com"
 
 export default axios.create({
   baseURL: BASE_URL,
