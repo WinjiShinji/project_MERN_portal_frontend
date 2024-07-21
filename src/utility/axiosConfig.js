41c59e476122654a5fdc0606db7003a4f655f572
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-07-21 10:43:40 +0100

    axios url

--- src/utility/axiosConfig.js
+++ src/utility/axiosConfig.js
@@ -1,18 +1,18 @@
-import axios from 'axios'
-const BASE_URL = 'http://localhost:3500'
+import axios from "axios"
+const BASE_URL = "52.41.36.82"
 
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
-    ContentType: 'application/json',
+    ContentType: "application/json",
   },
-  withCredentials: true // Allows httpCookies to be sent
+  withCredentials: true, // Allows httpCookies to be sent
 })
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: {
-    ContentType: 'application/json'
-  }, 
-  withCredentials: true // Allows httpCookies to be sent
+    ContentType: "application/json",
+  },
+  withCredentials: true, // Allows httpCookies to be sent
 })
